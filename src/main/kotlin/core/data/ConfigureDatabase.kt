0fcb1a3edcce49cebad1d2593bd661855011ac77
karim <sinouhkar@gmail.com>
2025-02-23 20:48:11 +0100

    some changes to the database configurations2

--- src/main/kotlin/core/data/ConfigureDatabase.kt
+++ src/main/kotlin/core/data/ConfigureDatabase.kt
@@ -6,9 +6,6 @@ import org.jetbrains.exposed.sql.Database
 fun Application.configureDatabase(){
 
     Database.connect(
-        url="jdbc:postgresql://appointments_whatsapp_ai_bot_user:1NnF9HaHdiDOA1NIE4HTQFA0gmD8O3Zj@dpg-cutmtcdsvqrc73e8t9f0-a.oregon-postgres.render.com:5432/appointments_whatsapp_ai_bot",
-        user = "appointments_whatsapp_ai_bot_user",
-        password = "1NnF9HaHdiDOA1NIE4HTQFA0gmD8O3Zj",
-        driver = "org.postgresql.Driver"
+        url="jdbc:postgresql://appointments_whatsapp_ai_bot_user:1NnF9HaHdiDOA1NIE4HTQFA0gmD8O3Zj@dpg-cutmtcdsvqrc73e8t9f0-a/appointments_whatsapp_ai_bot",
     )
 }
